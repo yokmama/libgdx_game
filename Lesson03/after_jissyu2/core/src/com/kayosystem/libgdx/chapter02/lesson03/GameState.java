e755c136a13f3af31ec8ac44de98f5e391af6924
Satoshi Kohno <s_kohno@kayosystem.com>
2017-01-20 18:14:26 +0900

    Lesson番号修正

--- Lesson03/after_jissyu2/core/src/com/kayosystem/libgdx/chapter02/lesson03/GameState.java
+++
@@ -1,8 +0,0 @@
-package com.kayosystem.libgdx.chapter02.lesson03;
-
-/**
- * ゲームの状態管理
- */
-public enum GameState {
-    Ready, Running, GameOver, GameCleared
-}
---
+++ Lesson05/after_jissyu2/core/src/com/kayosystem/libgdx/chapter02/lesson05/GameState.java
@@ -0,0 +1,8 @@
+package com.kayosystem.libgdx.chapter02.lesson05;
+
+/**
+ * ゲームの状態管理
+ */
+public enum GameState {
+    Ready, Running, GameOver, GameCleared
+}
